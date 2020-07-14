8d1acf8f0b1495f1c90603be564e957556808361
rverma510 <raghavverma4091@gmail.com>
2020-07-14 10:31:10 +0530

    added code for finding starting and ending node and implemented CalculateHValue

--- src/main.cpp
+++ src/main.cpp
@@ -62,7 +62,7 @@ int main(int argc, const char **argv)
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, start_x, start_x, end_x, end_y};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
